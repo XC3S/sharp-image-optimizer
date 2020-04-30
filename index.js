a0eae7ff914aeb1e8ffe787938b92a1571073ff0
Maximilian Geithe <maximilian.geithe@diva-e.com>
2020-04-30 09:11:01 +0200

    generate files with the correct target file extension

--- index.js
+++ index.js
@@ -4,16 +4,30 @@ const fs = require('fs');
 const config = {
     targets: [
         {
-            dirname: 'teaserDesktop',
+            dirname: 'home-stage-desktop',
             width: 1920,
             height: 750,
             quality: 90,
             progressive: true 
         },
         {
-            dirname: 'teaserMobile',
-            width: 500,
+            dirname: 'home-stage-mobile',
+            width: 767,
             height: 376,
+            quality: 90,
+            progressive: true 
+        },
+        {
+            dirname: 'teaser-stage-desktop',
+            width: 1920,
+            height: 430,
+            quality: 90,
+            progressive: true 
+        },
+        {
+            dirname: 'teaser-stage-mobile',
+            width: 767,
+            height: 430,
             quality: 90,
             progressive: true 
         }
@@ -21,7 +35,7 @@ const config = {
 }
 
 
-fs.readdir('images\\master', function(err, items) {
+fs.readdir('images/master', function(err, items) {
     if(err){
         console.error('[ERROR] ' + err);
         return
@@ -31,18 +45,18 @@ fs.readdir('images\\master', function(err, items) {
 
     config.targets.forEach(target => {
         items.forEach(item => {
-            targetDir = 'images\\' + target.dirname;
+            targetDir = 'images/' + target.dirname;
             if (!fs.existsSync(targetDir)) {
                 fs.mkdirSync(targetDir);
             }
-            sharp('images\\master\\' + item)
+            sharp('images/master/' + item)
                 .resize(target.width,target.height)
                 .removeAlpha()
                 .jpeg({ 
                     quality: target.quality,
                     progressive: target.progressive
                 })
-                .toFile('images\\' + target.dirname + '\\' + item, (err, info) => { 
+                .toFile('images/' + target.dirname + '/' + item.replace(/\.[^.]+$/, '.jpeg'), (err, info) => { 
                     if(err){
                         console.error('[ERROR]', err);
                     }
@@ -53,3 +67,9 @@ fs.readdir('images\\master', function(err, items) {
         });
     });
 });
+
+
+function getFileExtension(filename) {
+  var ext = /^.+\.([^.]+)$/.exec(filename);
+  return ext == null ? "" : ext[1];
+}
